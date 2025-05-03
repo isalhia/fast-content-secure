24ccc90ed18196d1729fd562610054a450b382bf
isalhia <ihsan@fastfilmsdubai.com>
2025-05-03 22:18:49 +0400

    Add files via upload

---
+++ pages/api/storyboard.js
@@ -0,0 +1,9 @@
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const { prompt } = req.body;
+  res.status(200).send(`Generated storyboard for: ${prompt}`);
+}
