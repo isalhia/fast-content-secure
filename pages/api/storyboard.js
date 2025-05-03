f5f89227397cfdb7c5ac25a51d2f262824777ccc
isalhia <ihsan@fastfilmsdubai.com>
2025-05-03 22:44:23 +0400

    Delete pages directory

--- pages/api/storyboard.js
+++
@@ -1,9 +0,0 @@
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
-  const { prompt } = req.body;
-  res.status(200).send(`Generated storyboard for: ${prompt}`);
-}
